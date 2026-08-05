fa6d866f32662b9cbd46b3ae79df4eb27e4e2072
Pahrolrozi1996 <mpahrolr@gmail.com>
2026-08-05 10:54:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1239,3 +1239,35 @@ document.querySelectorAll(".faq-question").forEach(button => {
     });
 
 });
+
+/* ==========================================
+MOBILE MENU
+========================================== */
+
+const menuToggle = document.querySelector(".menu-toggle");
+
+const navMenu = document.querySelector(".nav-menu");
+
+if(menuToggle){
+
+menuToggle.addEventListener("click",()=>{
+
+navMenu.classList.toggle("active");
+
+menuToggle.classList.toggle("active");
+
+});
+
+}
+
+document.querySelectorAll(".nav-menu a").forEach(link=>{
+
+link.addEventListener("click",()=>{
+
+navMenu.classList.remove("active");
+
+menuToggle.classList.remove("active");
+
+});
+
+});
