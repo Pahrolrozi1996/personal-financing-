ef7f20bc32772dc034ba416b075a4011703c3b84
Pahrolrozi1996 <mpahrolr@gmail.com>
2026-08-10 15:31:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1395,35 +1395,4 @@ tarikhInput.addEventListener("input", function () {
 });
 
 
-onClose: function(selectedDates, dateStr) {
-
-    if (!dateStr) {
-        document.getElementById("umur").value = "";
-        return;
-    }
-
-    // Jika pengguna taip sendiri, kira umur semula
-    const parts = dateStr.split("/");
-
-    if (parts.length !== 3) return;
-
-    const birth = new Date(parts[2], parts[1] - 1, parts[0]);
-    const today = new Date();
-
-    let age = today.getFullYear() - birth.getFullYear();
-
-    const monthDiff = today.getMonth() - birth.getMonth();
-
-    if (
-        monthDiff < 0 ||
-        (monthDiff === 0 && today.getDate() < birth.getDate())
-    ) {
-        age--;
-    }
-
-    document.getElementById("umur").value = age + " Tahun";
-
-}
-});
-
 
